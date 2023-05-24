23d814645b7e8a8fe38f31bd7af331d8a9134031
SamiraCavalcanti <86758007+SamiraCavalcanti@users.noreply.github.com>
2023-05-24 03:50:25 -0300

    Bancos de dados e SQL para Ciência de Dados

---
+++ LAB-IBM-Db2-Samira.sql
@@ -0,0 +1,37 @@
+
+
+CREATE TABLE
+  INSTRUTOR (
+    INS_ID INT PRIMARY KEY NOT NULL,
+    LASTNAME VARCHAR(50) NOT NULL,
+    FIRSTNAME VARCHAR(50) NOT NULL,
+    CITY VARCHAR (40),
+    COUNTRY CHAR(2)
+  );
+
+INSERT
+  INTO  INSTRUTOR
+(INS_ID, LASTNAME, FIRSTNAME, CITY, COUNTRY)
+  VALUES
+(1, 'AHUJA', 'RAV', 'TORONTO', 'CA') ,
+(2,'CHONG','RAUL','TORONTO','CA'),
+(3,'VASUDEVAN','HIMA','CHICAGO','US');
+
+SELECT *
+  FROM INSTRUTOR;
+
+ /* selecione o nome, sobrenome e país onde a cidade é Toronto */
+SELECT FIRSTNAME, LASTNAME, COUNTRY
+  FROM INSTRUTOR
+  WHERE CITY='TORONTO';
+/* atualize a linha de Rav Ahuja e mude sua cidade para Markham.*/
+
+UPDATE INSTRUTOR
+  SET CITY = 'MARKHAM '
+  WHERE INS_ID=1;
+  
+/* exclua a linha de Raul Chong da tabela.*/
+DELETE 
+FROM 
+INSTRUTOR
+  WHERE INS_ID=2
